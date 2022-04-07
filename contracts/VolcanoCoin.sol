76fd91de6819697e80fa03215090dd043f61192e
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-04-07 10:16:55 -0500

    contract: fix formatting

--- contracts/VolcanoCoin.sol
+++ contracts/VolcanoCoin.sol
@@ -4,96 +4,97 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 struct Payment {
-  uint amount;
-  address recipient;
+    uint256 amount;
+    address recipient;
 }
 
 contract VolcanoCoin {
-  address private owner;
-  uint private totalSupply;
-  uint16 private incrementAmount = 1000;
+    address private owner;
+    uint256 private totalSupply;
+    uint16 private incrementAmount = 1000;
 
-  mapping (address => uint) private balances;
-  mapping (address => Payment[]) private payments;
+    mapping(address => uint256) private balances;
+    mapping(address => Payment[]) private payments;
 
-  constructor(uint initialSupply) {
-    owner = msg.sender;
-    totalSupply = initialSupply;
-    balances[owner] = totalSupply;
-  }
+    constructor(uint256 initialSupply) {
+        owner = msg.sender;
+        totalSupply = initialSupply;
+        balances[owner] = totalSupply;
+    }
 
-// -- EVENTS -- //
+    // -- EVENTS -- //
 
-  event TotalSupplyChange(uint newTotalSupply);
+    event TotalSupplyChange(uint256 newTotalSupply);
 
-  event Transfer(address recipient, uint amount);
+    event Transfer(address recipient, uint256 amount);
 
-// -- MODIFIERS -- //
+    // -- MODIFIERS -- //
 
-  modifier requireOwner() {
-    require(
-        msg.sender == owner,
-        "Call must be made from owner account"
-    );
-    
-    _;
-  }
+    modifier requireOwner() {
+        require(msg.sender == owner, "Call must be made from owner account");
 
-// -- VIEW FUNCTIONS -- //
+        _;
+    }
 
-  function getOwner() public view  returns (address) {
-    return owner;
-  }
+    // -- VIEW FUNCTIONS -- //
 
-  function getTotalSupply() public view returns (uint) {
-    return totalSupply;
-  }
+    function getOwner() public view returns (address) {
+        return owner;
+    }
 
-  // TypeError: Data location must be "memory" or "calldata" for return parameter in function, but none was given.
-  // function getBalances() public returns (mapping (address => uint)) {
-  // balanceOf looks to be a standard name for this type of behavior
-  // https://ethereum.org/en/developers/docs/standards/tokens/erc-20/#methods
-  function balanceOf(address account) public view returns (uint) {
-    return balances[account];
-  }
+    function getTotalSupply() public view returns (uint256) {
+        return totalSupply;
+    }
 
-  // must return from memory: WHY?
-  // otherwise: TypeError: Data location must be "memory" or "calldata" for return parameter in function, but none was given.
-  function paymentsFrom(address account) public view returns (Payment[] memory) {
-    return payments[account];
-  }
+    // TypeError: Data location must be "memory" or "calldata" for return parameter in function, but none was given.
+    // function getBalances() public returns (mapping (address => uint)) {
+    // balanceOf looks to be a standard name for this type of behavior
+    // https://ethereum.org/en/developers/docs/standards/tokens/erc-20/#methods
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
+    }
 
-// -- MUTATING FUNCTIONS -- //
+    // must return from memory: WHY?
+    // otherwise: TypeError: Data location must be "memory" or "calldata" for return parameter in function, but none was given.
+    function paymentsFrom(address account)
+        public
+        view
+        returns (Payment[] memory)
+    {
+        return payments[account];
+    }
 
-  function increaseSupply() public requireOwner {
-    totalSupply += incrementAmount;
-    
-    emit TotalSupplyChange(totalSupply);
-  }
+    // -- MUTATING FUNCTIONS -- //
 
-  function transfer(address recipient, uint amount) public {
-    address sender = msg.sender;
-    uint currentBalance = balanceOf(msg.sender);
-    
-    require(
-      currentBalance >= amount,
-      "Transfer amount must be less than or equal to current balance of account"
-    );
-    
-    balances[sender] -= amount;
-    balances[recipient] += amount;
+    function increaseSupply() public requireOwner {
+        totalSupply += incrementAmount;
 
-    // only emit and record Payment after successful transfer
-    emit Transfer(recipient, amount);
+        emit TotalSupplyChange(totalSupply);
+    }
 
-    // working with arrays: https://docs.soliditylang.org/en/develop/types.html#arrays
-    // NOTE: the entry and the array value are pre-allocated? do not need to define as empty before being able to push
-    
-    // NOTE: NOT like js shorthand for object properties, payments[sender] = Payment({ recipient, amount });
-    payments[sender].push(Payment({ recipient: recipient, amount: amount }));
+    function transfer(address recipient, uint256 amount) public {
+        address sender = msg.sender;
+        uint256 currentBalance = balanceOf(msg.sender);
 
-    // NOTE: can also give struct fields in DEFINED order Payment { uint amount; address recipient; }
-    // treats the struct name like a constructor function with ordered params
-    // payments[sender].push(Payment(amount, recipient));
-  }
+        require(
+            currentBalance >= amount,
+            "Transfer amount must be less than or equal to current balance of account"
+        );
+
+        balances[sender] -= amount;
+        balances[recipient] += amount;
+
+        // only emit and record Payment after successful transfer
+        emit Transfer(recipient, amount);
+
+        // working with arrays: https://docs.soliditylang.org/en/develop/types.html#arrays
+        // NOTE: the entry and the array value are pre-allocated? do not need to define as empty before being able to push
+
+        // NOTE: NOT like js shorthand for object properties, payments[sender] = Payment({ recipient, amount });
+        payments[sender].push(Payment({recipient: recipient, amount: amount}));
+
+        // NOTE: can also give struct fields in DEFINED order Payment { uint amount; address recipient; }
+        // treats the struct name like a constructor function with ordered params
+        // payments[sender].push(Payment(amount, recipient));
+    }
 }
