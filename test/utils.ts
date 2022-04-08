940fa9d3cbf86f07295b17b7eb3076029035d17a
the-vampiire <25523682+the-vampiire@users.noreply.github.com>
2022-04-08 16:11:06 -0500

    test/utils: add getTestSigningAccounts, docs

--- test/utils.ts
+++ test/utils.ts
@@ -3,14 +3,54 @@ import { ethers } from "hardhat";
 import type { Signer } from "ethers";
 import { VolcanoCoin } from "../typechain";
 
-export const deployTestContract = async (ownerAccount: Signer) => {
+/**
+ * Deploys the VolcanoCoin contract and returns an instance of it for tests
+ * @param ownerAccount the signer that deploys the contract
+ * @param constructorArgs optional args to pass to .deploy()
+ */
+export const deployTestContract = async (
+  ownerAccount: Signer,
+  constructorArgs: any[] = []
+) => {
   const VolcanoCoin = await ethers.getContractFactory("VolcanoCoin");
 
   const volcanoCoinContract: VolcanoCoin = await VolcanoCoin.connect(
     ownerAccount
-  ).deploy();
+  ).deploy(...constructorArgs);
 
   await volcanoCoinContract.deployed();
 
   return volcanoCoinContract;
+};
+
+/**
+ * Destructure as many accounts as needed for tests
+ * @note the first signer is the default signing account (if none is specified when deploying or calling a contract)
+ *
+ * For just signers:
+ * @example
+ * ```js
+ * [[firstSigner], [secondSigner], ...] = await getTestSigningAccounts();
+ * ```
+ *
+ * For signers and their addresses
+ * @example
+ * ```js
+ * [
+ *  [firstSigner, firstSignerAddress],
+ *  [secondSigner, secondSignerAddress],
+ *  ...
+ * ] = await getTestSigningAccounts();
+ * ```
+ *
+ * @returns signer and address pairs
+ */
+export const getTestSigningAccounts: () => Promise<
+  [Signer, string][]
+> = async () => {
+  const signers = await ethers.getSigners();
+
+  return Promise.all(
+    signers.map(async (signer) => [signer, await signer.getAddress()])
+  );
 };
